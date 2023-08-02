788da27e67da2505f7c8465f1bede0bbba160baf
Indar Basto <indar.basto@gmail.com>
2023-08-02 10:42:27 +0300

    refactor: create auto height bootstrap

--- src/js/theme.js
+++ src/js/theme.js
@@ -2,28 +2,30 @@ window.addEventListener('DOMContentLoaded', () => {
   const lightIcon = document.querySelector('.lightToggleIcon');
   const darkIcon = document.querySelector('.darkToggleIcon');
 
-  const setTheme = themeName => {
-    lightIcon.style.display = themeName === 'theme-dark' ? 'none' : 'block';
-    darkIcon.style.display = themeName === 'theme-dark' ? 'block' : 'none';
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
-  };
-
-  const toggleTheme = () => {
-    const theme =
-      localStorage.getItem('theme') === 'theme-dark'
-        ? 'theme-light'
-        : 'theme-dark';
-    setTheme(theme);
-  };
-
-  (function () {
+  (() => {
     if (localStorage.getItem('theme') === 'theme-dark') {
-      setTheme('theme-dark');
+      setTheme('theme-dark', lightIcon, darkIcon);
     } else {
-      setTheme('theme-light');
+      setTheme('theme-light', lightIcon, darkIcon);
     }
     const buttonChange = document.querySelector('.theme-changer');
-    buttonChange.addEventListener('click', toggleTheme);
+    buttonChange.addEventListener('click', () =>
+      toggleTheme(lightIcon, darkIcon),
+    );
   })();
 });
+
+const setTheme = (themeName, lightIcon, darkIcon) => {
+  lightIcon.style.display = themeName === 'theme-dark' ? 'none' : 'block';
+  darkIcon.style.display = themeName === 'theme-dark' ? 'block' : 'none';
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+};
+
+const toggleTheme = (lightIcon, darkIcon) => {
+  const theme =
+    localStorage.getItem('theme') === 'theme-dark'
+      ? 'theme-light'
+      : 'theme-dark';
+  setTheme(theme, lightIcon, darkIcon);
+};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import 'normalize.css';
 import './scss/style.scss';
 
+import './js/bootstrap';
+
 //Theme controller
 import './js/theme';
 import './js/menu';
 import './js/language';
-import './js/utils';
